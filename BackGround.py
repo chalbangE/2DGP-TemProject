6d653c57279e4129b9796ca0ac4c6e250d8bfc2c
chalbangE <yubin41029@naver.com>
2023-10-20 02:13:01 +0900

    처음 게임을 켰을 때 시작화면이 나오도록 수정

--- BackGround.py
+++ BackGround.py
@@ -1,13 +1,22 @@
 from pico2d import *
 
+back_W, back_H = 752, 669
 
-class Background:
+def Click(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+class BackGround:
     def __init__(self):
-        self.image = load_image('PNG\play_background.png')
+        self.image = load_image('PNG\\ready_background.png')
     def draw(self):
-        self.image.draw(752 / 2, 669 / 2)
+        self.image.draw(back_W / 2, back_H / 2)
     def update(self):
         pass
 
-
-
+class Ready:
+    def __init__(self, back):
+        self.back = back
+    def draw(self):
+        self.image.draw(back_W / 2, back_H / 2)
+    def update(self):
+        pass
